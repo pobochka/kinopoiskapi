3529804583e11f1863f552b43140a8639f8d45ea
Pobochka <ivanandreiche2004@gmail.com>
2026-03-21 17:26:25 +0300

    fix_1

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://api.kinopoisk.dev",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-          configure: (proxy) => {
-            proxy.on("proxyReq", (proxyReq) => {
-              proxyReq.removeHeader("x-api-key");
-            });
-          },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.kinopoisk.dev",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy) => {
+          proxy.on("proxyReq", (proxyReq) => {
+            proxyReq.removeHeader("x-api-key");
+          });
         },
       },
     },
-  };
+  },
 });
